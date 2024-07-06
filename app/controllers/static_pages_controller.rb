6d318601ca80c9ea44762dcc181e5fb75864759f
seel <61373111+seelx3@users.noreply.github.com>
2024-07-07 02:22:11 +0900

    feat: add static page (#1)
    * feat: add static pages
    
    * test: add test (about page)
    
    * feat: add about page

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# StaticPagesController
+class StaticPagesController < ApplicationController
+  def home; end
+
+  def help; end
+
+  def about; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root "application#hello"
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  root 'application#hello'
 end
