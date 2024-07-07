52b3c6c02d7047c40b4275da601f73cab4495407
seelx3 <seelx3@gmail.com>
2024-07-07 16:28:41 +0900

    feat: full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper # rubocop:disable Style/Documentation
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
