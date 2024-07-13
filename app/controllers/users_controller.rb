c66fa5b76a476520baff3626d49c1ed1ee805b89
seelx3 <seelx3@gmail.com>
2024-07-13 19:07:34 +0900

    feat: show user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController # rubocop:disable Style/Documentation
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new; end
 end
