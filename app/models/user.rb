c6b30499cc3e0900afca905dc4ee6aad9150bbb0
seelx3 <seelx3@gmail.com>
2024-07-13 17:16:24 +0900

    feat: add user validations & tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
-class User < ApplicationRecord
+# frozen_string_literal: true
+
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+class User < ApplicationRecord # rubocop:disable Style/Documentation
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
 end
