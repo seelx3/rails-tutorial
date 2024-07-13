065be5bf4bec3a8751a7ebfcb9b7c2a5f9ba616d
seelx3 <seelx3@gmail.com>
2024-07-13 17:43:35 +0900

    feat: make lower case before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,9 @@
 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
 class User < ApplicationRecord # rubocop:disable Style/Documentation
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    uniqueness: true
 end
