6f4662010d50d85f877e5416d5223a09ed3bcef7
seelx3 <seelx3@gmail.com>
2024-07-14 01:27:19 +0900

    feat: add login page & sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController # rubocop:disable Style/Documentation
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy; end
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+module SessionsHelper # rubocop:disable Style/Documentation
+end
