e71077234538f9906604c34e8fc2c1fdad2a3868
seelx3 <seelx3@gmail.com>
2024-07-13 17:27:48 +0900

    feat: validation of uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
 class User < ApplicationRecord # rubocop:disable Style/Documentation
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
