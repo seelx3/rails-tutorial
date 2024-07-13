c6f77b84e851be7ac1038cc55e82d8d79948c612
seelx3 <seelx3@gmail.com>
2024-07-13 11:58:39 +0900

    fix: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   get  '/help', to: 'static_pages#help'
   get  '/about', to: 'static_pages#about'
