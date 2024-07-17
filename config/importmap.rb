d27ac3fa8adc432e67580c4b0115ee2a7103dd73
seelx3 <seelx3@gmail.com>
2024-07-18 05:48:32 +0900

    refactor

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,4 +7,4 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin_all_from "app/javascript/custom",      under: "custom"
+pin_all_from 'app/javascript/custom',      under: 'custom'
